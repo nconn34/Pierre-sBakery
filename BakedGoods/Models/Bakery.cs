4d906f8d13a0abe3d78fe0154881f390cf56088e
Nathan Conn <nconn34@gmail.com>
2022-02-12 19:28:22 -0800

    Wrote first console line

---
+++ BakedGoods/Models/Bakery.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Bakery.Logic
+{
+    public class BakedGoods
+    {
+        
+    }
+}
---
+++ BakedGoods/Models/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Bakery.Logic;
+
+namespace PierresBakery
+{
+    public class Program
+    {
+          public static void Main()
+     {
+         Console.WriteLine("Welcome to Pierre's Bakery!");
+     }
+    }
+}
